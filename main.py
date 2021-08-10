6a354b78a8536fa278489bb38d98e4e17dd8f891
kazuto28 <konndoukazuto45@gmail.com>
2021-08-10 10:41:36 +0900

    bug fix1

--- main.py
+++ main.py
@@ -2,26 +2,28 @@
 
 from mcpi import minecraft
 import os,sys,queue,json,time,threading
+from multiprocessing import Value
 
 
-def main(mc,q,wd):
-	os.chdir(wd)
-	sys.path.insert(1,wd)
+
+def main(mc,state):
+	root=os.path.dirname(os.path.abspath(__file__))+"/"
+	sys.path.insert(1,root)
 
 	rituals={}
 	altar={}
-	files=[os.path.splitext(i)[0] for i in os.listdir("ritual")]
+	files=[os.path.splitext(i)[0] for i in os.listdir(root+"ritual")]
 	for i in files:
 		if i or not r=="__pycache__":
 			rituals[i]=import_module(i,"ritual")
-		with open("altar/"+i,'r') as f:
+		with open(root+"altar/"+i,'r') as f:
 			altar[i]=json.load(f)
 
 	mc.postToChat("Welcome to Minecraft Pi Altars.")
 
 	mc.events.clearAll()
 	try:
-		while q.get()==1:
+		while state.value:
 			events=mc.events.pollBlockHits()
 			for e in events:
 				if mc.getBlock(e.pos.x,e.pos.y,e.pos.z)==247:
@@ -45,17 +47,17 @@ def main(mc,q,wd):
 
 if __name__=="__main__":
 	mc=minecraft.Minecraft.create()
-	qu=queue.Queue()
-	thread=threading.Thread(target=main,args=(mc,qu,os.getcwd()))
+	v=Value("i",1)
+	thread=threading.Thread(target=main,args=(mc,v))
 	thread.start()
 	while True:
 		try:
 			com=input("> ")
 			if com=="exit":
-				qu.put(0)
+				v.value=0
 				thread.join()
 				sys.exit()
 		except KeyboardInterrupt:
-			qu.put(0)
+			v.value=0
 			thread.join()
 			sys.exit()
