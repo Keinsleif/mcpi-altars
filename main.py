79ac1866aed404693f9d5b85bcc47c947cf383a2
kazuto28 <konndoukazuto45@gmail.com>
2021-08-10 10:45:41 +0900

    bug fix3

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main(mc,state):
 	files=[os.path.splitext(i)[0] for i in os.listdir(root+"ritual")]
 	for i in files:
 		if i or not r=="__pycache__":
-			rituals[i]=import_module(i,"ritual")
+			rituals[i]=import_module("ritual."+i)
 		with open(root+"altar/"+i,'r') as f:
 			altar[i]=json.load(f)
 
