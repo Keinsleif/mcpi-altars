3a0ab225d7e6593efbd08bed9099331567662eb9
kazuto28 <konndoukazuto45@gmail.com>
2021-08-10 11:18:40 +0900

    bug fix4

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ def main(mc,state):
 
 	rituals={}
 	altar={}
-	files=[os.path.splitext(i)[0] for i in os.listdir(root+"ritual")]
+	files=[os.path.splitext(i)[0] for i in os.listdir(root+"altar")]
 	for i in files:
-		if i or not r=="__pycache__":
 			rituals[i]=import_module("ritual."+i)
-		with open(root+"altar/"+i,'r') as f:
-			altar[i]=json.load(f)
+			with open(root+"altar/"+i,'r') as f:
+				altar[i]=json.load(f)
 
 	mc.postToChat("Welcome to Minecraft Pi Altars.")
 
@@ -29,9 +28,9 @@ def main(mc,state):
 			for e in events:
 				if mc.getBlock(e.pos.x,e.pos.y,e.pos.z)==247:
 					al=[mc.getBlock(x,y,z) for y in range(e.pos.y,e.pos.y+3) for x in range(e.pos.x-1,e.pos.x+2) for z in range(e.pos.z-1,e.pos.z+2)]
-					for i in files:
+					for i in altar:
 						for a in altar[i]:
-							if al==altar[a]['altar']:
+							if al==altar[i]	[a]['altar']:
 								mc.postToChat('Success')
 								exec(altar[i][a]["function"].format(ritual="rituals['"+i+"']"))
 								mc.postToChat('Stopped')
@@ -43,7 +42,7 @@ def main(mc,state):
 					else:
 						mc.postToChat('Ritual not found.')
 			time.sleep(0.5)
-	except KeyboardInterrupt:
+	except (KeyboardInterrupt,EOFError):
 		print()
 
 if __name__=="__main__":
@@ -58,7 +57,7 @@ if __name__=="__main__":
 				v.value=0
 				thread.join()
 				sys.exit()
-		except KeyboardInterrupt:
+		except (KeyboardInterrupt,EOFError):
 			v.value=0
 			thread.join()
 			sys.exit()
